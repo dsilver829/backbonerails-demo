c28e48240b6738a0301875a904167c48fbf601f2
David Silver <dsilver@candidatemetrics.com>
2013-05-18 19:03:13 -0700

    setting up

--- README.md
+++
@@ -1,2 +0,0 @@
-backbonerails-demo
-==================
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BackbonerailsDemo::Application.config.secret_token = 'f9455b2ae63356382f3c86dc9a9671bc5e0a0ba20be6bbf5c3c09af239bea91576ae33fb13cfb6b3f380b57826cb15ad6a34a3c3e17704276fe79aaea096edf9'
