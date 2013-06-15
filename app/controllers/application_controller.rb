3308c806852d03fef9e528b82947cfdbf1f4b066
David Silver <dsilver@candidatemetrics.com>
2013-06-15 11:08:18 -0700

    set up current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def index
-
+    @user = User.first # example
+    gon.rabl "app/views/users/show.json.rabl", as: 'current_user'
   end
 end
